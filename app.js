bf709734b9ac5e8b8754e8ffdc5c39222e8680de
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-11-05 22:47:23 +0300

    HW 3 - Add Post / Show Post

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express'); // import express
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post');
+
 const app = express(); // start express
+
+// connect db
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 // template engine
 app.set('view engine', 'ejs');
 
 // middleware
 app.use(express.static('public')); // static web
+app.use(express.urlencoded({ extended: true })); // reading post
+app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', { posts });
 });
 
 app.get('/about', (req, res) => {
@@ -19,6 +28,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 // port
